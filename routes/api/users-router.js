8bd88ff092a874654321e97b8d6a277469857183
Serhii Deineha <sergdd24041997@gmail.com>
2022-07-18 06:48:54 -0400

    hm5

--- routes/api/users-router.js
+++ routes/api/users-router.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { asyncWrapper } = require("../../helpers/api-helpers");
 const { validationUser } = require("../../middlewares/validation-middleware");
 const { authMiddleware } = require("../../middlewares/auth-middleware");
+const { uploadMiddleware } = require("../../middlewares/upload-middlewar");
 const {
   registrationController,
   loginController,
   logoutController,
   currentUserController,
   changesSubscriptionController,
+  updateAvatarController,
 } = require("../../controllers/api/users-controller");
 
 const router = express.Router();
@@ -22,5 +24,12 @@ router.get("/logout", authMiddleware, asyncWrapper(logoutController));
 router.get("/current", authMiddleware, asyncWrapper(currentUserController));
 // changes subscription user
 router.patch("/", authMiddleware, asyncWrapper(changesSubscriptionController));
+// uploads an avatar
+router.patch(
+  "/avatars",
+  authMiddleware,
+  uploadMiddleware.single("avatar"),
+  asyncWrapper(updateAvatarController)
+);
 
 module.exports = router;
